7623286207052f4234cc9f00e20429ad774a36f4
milek oleszczak <milek.oleszczak@hotmail.com>
2023-08-26 16:02:21 +0200

    tests

--- Stories.Tests/StoryDetailsClient.Tests.cs
+++ Stories.Tests/StoryDetailsClient.Tests.cs
@@ -7,113 +7,135 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using FluentAssertions;
 using Stories.Model;
-using Flurl.Http;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Stories.Tests
 {
     public class StoryDetailsClientTests
     {
-		private string apiUrl = "https://dummy.com";
-		private HttpTest _httpTest;
-		private ServiceProvider serviceProvider;
+        private string apiUrl = "https://dummy.com";
+        private HttpTest _httpTest;
+        private ServiceProvider serviceProvider;
 
-		[SetUp]
-		public void Setup()
-		{
-			_httpTest = new HttpTest();
+        [SetUp]
+        public void Setup()
+        {
+            _httpTest = new HttpTest();
 
-			var services = new ServiceCollection();
+            var services = new ServiceCollection();
 
-			services.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+            services.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
 
-			var mockConfiguration = Substitute.For<IStoriesApiConfiguration>();
-			mockConfiguration.Url.Returns(this.apiUrl);
+            var mockConfiguration = Substitute.For<IStoriesApiConfiguration>();
+            mockConfiguration.Url.Returns(this.apiUrl);
 
-			services.TryAddSingleton((sp) => mockConfiguration);
-			services.TryAddScoped<IStoryDetailsApiClient, StoryDetailsApiClient>();
-			services.TryAddSingleton<WaitDurationProvider>((attempt) => TimeSpan.FromMilliseconds(1));
+            services.TryAddSingleton((sp) => mockConfiguration);
+            services.TryAddScoped<IStoryDetailsApiClient, StoryDetailsApiClient>();
+            services.TryAddSingleton<WaitDurationProvider>((attempt) => TimeSpan.FromMilliseconds(1));
+            services.TryAddSingleton<CacheExpirationProvider>(() => TimeSpan.FromSeconds(10));
+
+            services.TryAddScoped(typeof(IApiClient<Story>), typeof(ApiClient<Story>));
+            services.TryDecorate(typeof(IApiClient<Story>), typeof(ApiClientWithCache<Story>));
+
+            var mockMemoryCache = Substitute.For<IMemoryCache>();
+            services.TryAddSingleton(mockMemoryCache);
 
             services.AddAutoMapper(typeof(MappingConfiguration));
 
-            var mockMemoryCache = Substitute.For<IMemoryCache>();
-			services.TryAddSingleton(mockMemoryCache);
+            this.serviceProvider = services.BuildServiceProvider();
+        }
 
-			this.serviceProvider = services.BuildServiceProvider();
-		}
+        [TearDown]
+        public void DisposeHttpTest()
+        {
+            _httpTest.Dispose();
+        }
 
-		[TearDown]
-		public void DisposeHttpTest()
-		{
-			_httpTest.Dispose();
-		}
+        [Test]
+        public async Task Should_Return_StoryDetails_Success()
+        {
+            string urlPattern = $"{this.apiUrl}/item/*";
 
-		//[Test]
-		//public async Task Should_Return_StoryDetails_Success()
-		//{
-		//	string urlPattern = $"{this.apiUrl}/item/*";
+            _httpTest
+                .ForCallsTo(urlPattern)
+                .RespondWithJson(SampleStories[1]);
 
-		//	_httpTest
-		//		.ForCallsTo(urlPattern)
-		//		.RespondWithJson(SampleStories[1]);
+            var client = this.serviceProvider.GetRequiredService<IStoryDetailsApiClient>();
 
-		//	var client = this.serviceProvider.GetRequiredService<IStoryDetailsApiClient>();
+            var response = await client.GetDetails(new[] { 1 }, CancellationToken.None).ToListAsync();
 
-		//	var response = await client.GetDetails(1, CancellationToken.None);
+            response.Should().BeEquivalentTo(new[] { SampleStoriesDto[1] });
+        }
 
-		//	response.Should().BeEquivalentTo(SampleStories[1]);
-		//}
+        [Test]
+        public async Task Should_Return_StoryDetails_After3Retries()
+        {
+            string urlPattern = $"{this.apiUrl}/item/*";
 
-		//[Test]
-		//public async Task Should_Return_StoryDetails_After3Retries()
-		//{
-		//	string urlPattern = $"{this.apiUrl}/item/*";
+            _httpTest
+                .ForCallsTo(urlPattern)
+                .RespondWith("Client failure", 404)
+                .RespondWithJson(SampleStories[1])
+                .RespondWith("Server failure", 500)
+                .RespondWithJson(SampleStories[2]);
 
-		//	_httpTest
-		//		.ForCallsTo(urlPattern)
-		//		.RespondWith("Client failure", 404)
-		//		.RespondWith("Server failure", 500)
-		//		.RespondWithJson(SampleStories[1]);
+            var client = this.serviceProvider.GetRequiredService<IStoryDetailsApiClient>();
 
-		//	var client = this.serviceProvider.GetRequiredService<IStoryDetailsApiClient>();
+            var response = await client.GetDetails(new[] { 1, 2 }, CancellationToken.None).ToListAsync();
 
-		//	var response = await client.GetDetails(1, CancellationToken.None);
+            response.Should().BeEquivalentTo(SampleStoriesDto.Values);
 
-		//	response.Should().BeEquivalentTo(SampleStories[1]);
-		//	_httpTest.ShouldHaveCalled(urlPattern).Times(3);
-		//}
+            _httpTest.ShouldHaveCalled(urlPattern).Times(4);
+        }
 
-		//[Test]
-		//public async Task Should_Faild_And_Raise_Exception()
-		//{
-		//	string urlPattern = $"{this.apiUrl}/item/*";
 
-		//	_httpTest
-		//		.ForCallsTo(urlPattern)
-		//		.RespondWith("Client failure", 404)
-		//		.RespondWith("Client failure", 401)
-		//		.RespondWith("Server failure", 500)
-		//		.RespondWithJson(SampleStories[1]);
+        private static Dictionary<int, Story> SampleStories = new Dictionary<int, Story>
+        {
+            {
+                1,
+                new Story()
+                {
+                    Id = 1,
+                    Title = "story 1",
+                    By = "XXX",
+                    Type = "story",
+                }
+            },
+            {
+                2,
+                new Story()
+                {
+                    Id = 2,
+                    Title = "story 2",
+                    By = "YYY",
+                    Type = "story",
+                    Kids = new[] { 10001, 10002, 10003 },
+                }
+            },
+        };
 
-		//	var client = this.serviceProvider.GetRequiredService<IStoryDetailsApiClient>();
-
-		//	var sendRequest = async () => await client.GetDetails(1, CancellationToken.None);
-
-		//	await sendRequest.Should().ThrowAsync<FlurlHttpException>();
-
-		//}
-
-		private static Dictionary<int, Story> SampleStories = new Dictionary<int, Story>
-		{
-			{ 
-				1, 
-				new Story() 
-				{
-					Id = 1,
-					By = "XXX",
-					Type = "story",
-				} 
-			},
-		};
-	}
+        private static Dictionary<int, StoryDto> SampleStoriesDto = new Dictionary<int, StoryDto>
+        {
+            {
+                1,
+                new StoryDto()
+                {
+                    Title = "story 1",
+                    By = "XXX",
+                    Type = "story",
+                    CommentsCount = 0,
+                }
+            },
+            {
+                2,
+                new StoryDto()
+                {
+                    Title = "story 2",
+                    By = "YYY",
+                    Type = "story",
+                    CommentsCount = 3,
+                }
+            },
+        };
+    }
 }
